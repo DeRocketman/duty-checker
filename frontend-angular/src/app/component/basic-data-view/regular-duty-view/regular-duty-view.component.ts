4390c8cc277552f2e33eef1623bd5f290d11a4d1
stricker <de.rocketman@outlook.com>
2023-02-07 19:44:17 +0100

    duty import

--- frontend-angular/src/app/component/basic-data-view/regular-duty-view/regular-duty-view.component.ts
+++ frontend-angular/src/app/component/basic-data-view/regular-duty-view/regular-duty-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Duty} from "../../../shared/duty";
 import {DutyService} from "../../../service/duty.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'dutch-regular-duty-view',
@@ -12,7 +13,7 @@ export class RegularDutyViewComponent implements OnInit {
   headerColumns: string[] = ["Name", "Dienstbeginn", "Dienstende", "Pausenlänge", "Bezahlte Zeit", "Brutto Zeit", "Anzahl d. Beschr."]
   regularDuties: Duty[] = [];
 
-  constructor(private dutyService: DutyService) { }
+  constructor(private dutyService: DutyService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.dutyService.getAll().subscribe(
@@ -21,6 +22,31 @@ export class RegularDutyViewComponent implements OnInit {
   }
 
   importRegularDuties(e: any): void {
+    let file = e.target.files[0];
+    console.log(file)
+    // @ts-ignore
+    let reader : FileReader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      let csv: any = reader.result;
+      let allTextLines = csv.split(/\r\n|\n/)
+      let headers = allTextLines[0].split(";")
+      console.log(headers)
 
+      let tarrR = [];
+
+      let arrl = allTextLines.length;
+      let rows = [];
+      for(let i = 1; i < arrl; i++){
+        rows.push(allTextLines[i].split(';'));
+      }
+      console.log(rows);
+      for (let j = 0; j < arrl; j++) {
+        tarrR.push(rows[j]);
+      }
+      console.log(tarrR);
+      //Push rows to array variable
+
+    }
   }
 }
