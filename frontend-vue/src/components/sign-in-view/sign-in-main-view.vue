ee039062dfb506a78d90ac1f0bff2d2cc6f4c791
stricker <de.rocketman@outlook.com>
2023-02-13 22:20:00 +0100

    router add all auth guard add angular

--- frontend-vue/src/components/sign-in-view/sign-in-main-view.vue
+++ frontend-vue/src/components/sign-in-view/sign-in-main-view.vue
@@ -1,5 +1,63 @@
 <template>
-
+    <div class="col-md-12">
+    <div class="card card-container">
+      <img
+        id="profile-img"
+        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+        class="profile-img-card"
+      />
+      <form name="form" @submit.prevent="handleSignIn">
+        <div class="form-group">
+          <label for="email">E-Mail</label>
+          <input
+            v-model="user.email"
+            v-validate="'required'"
+            type="email"
+            class="form-control"
+            name="email"
+          />
+          <div
+            v-if="errors.has('email')"
+            class="alert alert-danger"
+            role="alert"
+          >
+            E-Mail ist notwendig!
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="password">Passwort</label>
+          <input
+            v-model="user.password"
+            v-validate="'required'"
+            type="password"
+            class="form-control"
+            name="password"
+          />
+          <div
+            v-if="errors.has('password')"
+            class="alert alert-danger"
+            role="alert"
+          >
+            Passwort ist notwendig!
+          </div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary btn-block" :disabled="loading">
+            <span
+              v-show="loading"
+              class="spinner-border spinner-border-sm"
+            ></span>
+            <span>Anmelden</span>
+          </button>
+        </div>
+        <div class="form-group">
+          <div v-if="message" class="alert alert-danger" role="alert">
+            {{ message }}
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
